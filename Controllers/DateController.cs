e9cc81446446ac5d3209a4a72da785968e7bf17d
artemya <artemya@tahometer.com>
2020-10-26 21:22:11 +0200

    init

---
+++ Controllers/DateController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using lab1_1.Models;
+
+namespace lab1_1.Controllers
+{
+    public class DateController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string name, DateTime date)
+        {
+            string result = $"Name: {name}   Date: {DateTime.Now - date} {date}";
+            return Content(result);
+        }
+
+    }
+}
